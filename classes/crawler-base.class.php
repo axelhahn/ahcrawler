f5ffc76f2957385013fff476ff078dc8e51a8071
Axel Hahn <ax2002@gmx.net>
2019-04-24 23:56:07 +0200

    2019-04-24: v0.71   * backend: update font-awesome/5.8.1, jquery/3.4.0, Chart.js/2.8.0   * backend: fix texts in updater   * backend: fix bookmarklet url to scan all profiles

--- classes/crawler-base.class.php
+++ classes/crawler-base.class.php
@@ -14,7 +14,7 @@ class crawler_base {
     public $aAbout = array(
         'product' => 'ahCrawler',
         'version' => '0.71',
-        'date' => '2019-04-23',
+        'date' => '2019-04-24',
         'author' => 'Axel Hahn',
         'license' => 'GNU GPL 3.0',
         'urlHome' => 'https://www.axel-hahn.de/ahcrawler',
