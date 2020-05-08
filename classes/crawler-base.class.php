e2a5f40fd98359ae11f08bf27e99e4391cc53913
Axel Hahn <ax2002@gmx.net>
2020-05-08 22:43:39 +0200

    2020-05-09: v0.108   * UPDATE: database table was changed     execute "php bin/cli.php -a flush -d searchindex"     and then index your projects again   * UPDATE: show count of words in title, keywords, description   * UPDATE: remove PHP Deprecated: mb_strrpos() in analyzer.html.class.php (PHP 7.4)   * FIX: htmlchecks page: show number of pages (it was broken in 1.06)

--- classes/crawler-base.class.php
+++ classes/crawler-base.class.php
@@ -32,8 +32,8 @@ class crawler_base {
 
     public $aAbout = array(
         'product' => 'ahCrawler',
-        'version' => '0.108-dev',
-        'date' => '2020-05-xx',
+        'version' => '0.108',
+        'date' => '2020-05-09',
         'author' => 'Axel Hahn',
         'license' => 'GNU GPL 3.0',
         'urlHome' => 'https://www.axel-hahn.de/ahcrawler',
