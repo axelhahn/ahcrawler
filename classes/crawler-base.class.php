30c15ca4b9df4a8d968749c900f67b7fed363f41
Axel Hahn <ax2002@gmx.net>
2020-06-21 23:13:47 +0200

    2020-06-21: v0.121   * UPDATE: header "Public-Key-Pins" is marked as deprecated   * UPDATE: header "X-Frame-Options" marks ALLOW-FROM as warning   * UPDATE: css - boxes in overview pages   * UPDATE: header in ressource details links to live http header check (public page must be enabled)

--- classes/crawler-base.class.php
+++ classes/crawler-base.class.php
@@ -32,8 +32,8 @@ class crawler_base {
 
     public $aAbout = array(
         'product' => 'ahCrawler',
-        'version' => '0.121-dev',
-        'date' => '2020-06-xx',
+        'version' => '0.121',
+        'date' => '2020-06-21',
         'author' => 'Axel Hahn',
         'license' => 'GNU GPL 3.0',
         'urlHome' => 'https://www.axel-hahn.de/ahcrawler',
