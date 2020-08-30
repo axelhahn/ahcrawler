9a01adb381f373afccf0fdf485dc0ed574864ff2
Axel Hahn <ax2002@gmx.net>
2020-08-30 21:12:04 +0200

    2020-08-30: v0.132   * ADDED: show warnings for deprecated http headers   * UPDATE: forms in profiles and settings   * UPDATE: use password_hash() instead of md5() for login. If you used the             build in user ... In config/crawler.config.json ...             remove the entry options -> auth -> user.             Then go to the settings in the backend to set the user and password again.             OR             Get a new password hash by             > php -r "echo password_hash('mypassword', PASSWORD_DEFAULT);"             and enter the output into options -> auth -> password   * FIX: navigating from "add profile" page to any other page

--- classes/crawler-base.class.php
+++ classes/crawler-base.class.php
@@ -32,8 +32,8 @@ class crawler_base {
 
     public $aAbout = array(
         'product' => 'ahCrawler',
-        'version' => '0.131',
-        'date' => '2020-08-19',
+        'version' => '0.132',
+        'date' => '2020-08-30',
         'author' => 'Axel Hahn',
         'license' => 'GNU GPL 3.0',
         'urlHome' => 'https://www.axel-hahn.de/ahcrawler',
