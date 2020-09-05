010e24282050fc21c01d42383eef20396a973cb0
Axel Hahn <ax2002@gmx.net>
2020-09-05 13:35:53 +0200

    2020-09-05: v0.133   * ADDED: profile image - has a delete button and file upload too now   * FIX: index ressources with more pages with sqlite engine   * FIX: searchindex indexer could have false positives in extension detection   * FIX: cli calls "-a reindex" or "-a index -d all" with sqlite engine locked the database for ressources   * UPDATE: cli - show hint if using "-d all"   * UPDATE: searchindex indexer got a few more extensions   * UPDATE: profile image uses jpeg insted of png (uses less space)   * UPDATE: wording changed: blacklist into deny list

--- classes/crawler-base.class.php
+++ classes/crawler-base.class.php
@@ -32,8 +32,8 @@ class crawler_base {
 
     public $aAbout = array(
         'product' => 'ahCrawler',
-        'version' => '0.133-dev',
-        'date' => '2020-09-xx',
+        'version' => '0.133',
+        'date' => '2020-09-05',
         'author' => 'Axel Hahn',
         'license' => 'GNU GPL 3.0',
         'urlHome' => 'https://www.axel-hahn.de/ahcrawler',
