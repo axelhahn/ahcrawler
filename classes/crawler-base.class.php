6445090e2dc7ef17ff1480c6fae381553e405bb5
Axel Hahn <ax2002@gmx.net>
2020-09-23 14:21:29 +0200

    2020-09-23: v0.135   * ADD: log cli output of crawling actions in ./data/   * ADD: page to view log data of crawling actions   * FIX: ressource scan shows matching regex of the deny list   * FIX: profile page layout error   * UPDATE: show hint if a url matches a regex in the deny list   * UPDATE: show hint if a url switches from https to http

--- classes/crawler-base.class.php
+++ classes/crawler-base.class.php
@@ -32,8 +32,8 @@ class crawler_base {
 
     public $aAbout = array(
         'product' => 'ahCrawler',
-        'version' => '0.135-dev',
-        'date' => '2020-09-xx',
+        'version' => '0.135',
+        'date' => '2020-09-23',
         'author' => 'Axel Hahn',
         'license' => 'GNU GPL 3.0',
         'urlHome' => 'https://www.axel-hahn.de/ahcrawler',
