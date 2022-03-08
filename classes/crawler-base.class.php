7971164a561c7fed7c45d6e683004ee13682ad8b
Axel Hahn <ax2002@gmx.net>
2022-03-08 23:52:15 +0100

    set dev version

--- classes/crawler-base.class.php
+++ classes/crawler-base.class.php
@@ -33,8 +33,8 @@ class crawler_base {
 
     public $aAbout = array(
         'product' => 'ahCrawler',
-        'version' => '0.151',
-        'date' => '2022-03-07',
+        'version' => '0.152_dev',
+        'date' => '2022-03-xx',
         'author' => 'Axel Hahn',
         'license' => 'GNU GPL 3.0',
         'urlHome' => 'https://www.axel-hahn.de/ahcrawler',
