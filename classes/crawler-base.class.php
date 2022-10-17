b9b21781cfb77a4c71349442ac2aa511e49f6524
Axel Hahn <ax2002@gmx.net>
2022-10-18 00:47:54 +0200

    set date of release to Oct

--- classes/crawler-base.class.php
+++ classes/crawler-base.class.php
@@ -34,7 +34,7 @@ class crawler_base {
     public $aAbout = array(
         'product' => 'ahCrawler',
         'version' => '0.154-dev',
-        'date' => '2022-09-xx',
+        'date' => '2022-10-??',
         'author' => 'Axel Hahn',
         'license' => 'GNU GPL 3.0',
         'urlHome' => 'https://www.axel-hahn.de/ahcrawler',
