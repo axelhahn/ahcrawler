bc669decf7edf73773f1571a4a2c1c51e980aa30
Axel Hahn <ax2002@gmx.net>
2022-10-21 23:27:10 +0200

    deselect OK status buttons on linked resources

--- backend/javascript/functions-ressourcedetail.js
+++ backend/javascript/functions-ressourcedetail.js
@@ -6,9 +6,9 @@
 // on start: 
 window.addEventListener('load', function() {
 
-    // deselect OK status buttons
-    $('a.http-code-ok').removeClass('text-on-markedelement http-code-2xx http-code-ok');
+    // deselect OK status buttons on linked resources
+    $('#div-toggle-3 a.http-code-ok').removeClass('text-on-markedelement http-code-2xx http-code-ok');
 
-    // hide ok resources
-    $('div.group-2xx-ok').hide();
+    // hide ok items on linked resources
+    $('#div-toggle-3 div.group-2xx-ok').hide();
 });
