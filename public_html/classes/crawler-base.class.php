43a589f09cd475efa6626acb274dd847530e290c
Axel Hahn <ax2002@gmx.net>
2023-05-09 23:19:23 +0200

    set version

--- public_html/classes/crawler-base.class.php
+++ public_html/classes/crawler-base.class.php
@@ -33,8 +33,8 @@ class crawler_base {
 
     public $aAbout = array(
         'product' => 'ahCrawler',
-        'version' => '0.161-orso',
-        'date' => '2023-05-07',
+        'version' => '0.161',
+        'date' => '2023-05-09',
         'author' => 'Axel Hahn',
         'license' => 'GNU GPL 3.0',
         'urlHome' => 'https://www.axel-hahn.de/ahcrawler',
