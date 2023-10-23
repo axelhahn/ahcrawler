bb53f5a7c385bffc0ad9eb005d33c21e6a6cffc0
Axel Hahn <ax2002@gmx.net>
2023-10-23 23:28:58 +0200

    counters: buttons instead of select box

--- public_html/backend/pages/counters.php
+++ public_html/backend/pages/counters.php
@@ -39,45 +39,11 @@ if (!$aList){
     # TODO translate
     $sHtml='INFO: no counter yet. They appear, if you start crawling.';
 } else {
-    $aCounterOptions=[];
     foreach($aList as $sLabel){
-        $aOptionItem=[
-            'label'=>$sLabel,
-            'value'=>$sLabel,
-        ];
-        if ($sSelectedCounter == $sLabel) {
-            $aOptionItem['selected']='selected';
-        }
-        $aCounterOptions[]=$aOptionItem;
+        $sSelector.='<a href="?page=counters&siteid='.$iProfileId.'&countername='.$sLabel.'" 
+            class="pure-button '.($sSelectedCounter == $sLabel ? ' button-secondary' : '').'" 
+            style="width: 100%;">'.$sLabel.'</a><br>';
     }
-
-    $sSelId='countervalues';
-    $sSelector.= $this->lB('counter.select') . ':<br><br><form action="" method="get" class="pure-form">'
-        . $oRenderer->oHtml->getTag('input', array(
-            'type'=>'hidden',
-            'name'=>'page',
-            'value'=>'counters',
-            ), false)
-            . $oRenderer->oHtml->getTag('input', array(
-                'type'=>'hidden',
-                'name'=>'siteid',
-                'value'=>$iProfileId,
-                ), false)
-        
-        
-        . '<div class="pure-control-group">'
-        // . $oRenderer->oHtml->getTag('label', array('for'=>$sSelId, 'label'=>$this->lB('setup.section.backend.lang')))
-        . $oRenderer->oHtml->getFormSelect(array(
-            'id'=>$sSelId, 
-            'name'=>'countername',
-            'size'=>count($aList) > $iSizeOfSelectbox ? $iSizeOfSelectbox : count($aList),
-            'onchange'=>'form.submit();'
-            ), $aCounterOptions)
-        . '</div>'
-        . '<noscript><br><button class="pure-button">' . $this->_getIcon('button.view') . $this->lB('button.view') . '</button></noscript>'
-        .'</form>'
-    ;
-
 }
 
 // ---------- load data of a selected counter
@@ -91,15 +57,10 @@ if($sSelectedCounter){
     $sGraph.=$this->_getHistoryCounter([$sSelectedCounter])
         . $this->_getHtmlTable($aTable, 'counter.', 'tblCounterdata')
         // . '<pre>'.print_r($oCounter->getCountersHistory($sSelectedCounter), 1).'</pre>'
-    ;
-
-    // foreach($oCounter->getCountersHistory($sCItem)
-    //$this->_getHtmlTable($aTable, $sLangTxtPrefix, $sTableId)
-    
+    ;    
 }
 
 // ---------- complete output
 
 $sHtml.='<table><th><tr><td valign="top">'.$sSelector.'</td><td>&nbsp;&nbsp;&nbsp;</td><td valign=top>'.$sGraph.'</td></tr></th></table>';
-    // $sHtml.=$sSelector.$sGraph;
 return $sHtml;
