ec73f30076094d82df26bff03179dce745a9e59c
Axel Hahn <ax2002@gmx.net>
2024-09-10 00:25:04 +0200

    update status and ssl nfos class for php8 only

--- public_html/classes/status.class.php
+++ public_html/classes/status.class.php
@@ -22,72 +22,89 @@
  * ----------------------------------------------------------------------------
  * status ... set, read, delete status of a running action to prevent
  * multiple simultanous actions
- * 2017-04-nn  created
+ * 
+ * 2017-04-nn          created
+ * 2024-09-10  v0.167  php8 only; add typed variables; use short array syntax
  **/
-class status {
+class status
+{
 
-    protected $_iTimeout = 10; // value in sec
+    /**
+     * Timeout in sec
+     * @var int
+     */
+    protected int $_iTimeout = 10; // value in sec
 
-    protected $_aMsg=array();
+    /**
+     * List of messages
+     * @var array
+     */
+    protected array $_aMsg = [];
 
     // ----------------------------------------------------------------------
 
     /**
-     * new status
+     * Constructor - new status
      */
-    public function __construct() {
-        return true;
+    public function __construct()
+    {
     }
-    
+
     // ----------------------------------------------------------------------
     // PRIVATE
     // ----------------------------------------------------------------------
 
     /**
-     * get a (maybe) currently running action and return the id of it; 
+     * Get a (maybe) currently running action and return the id of it; 
      * it returns false or the name of the action
-     * @return boolean
+     * 
+     * @return boolean|string
      */
-    public function _getAction(){
-        $sFile=$this->_getTouchfile();
-        
+    public function _getAction(): bool|string
+    {
+        $sFile = $this->_getTouchfile();
+
         // if there is no action then return false
-        if (!file_exists($sFile)){
+        if (!file_exists($sFile)) {
             return false;
         }
         // if file is too old return false
-        $aTmp=stat($sFile);
-        if (date("U")-$aTmp['mtime'] > $this->_iTimeout){
+        $aTmp = stat($sFile);
+        if (date("U") - $aTmp['mtime'] > $this->_iTimeout) {
             $this->_delete();
             return false;
         }
         // 
-        $aMsg=json_decode(file_get_contents($sFile), 1);
+        $aMsg = json_decode(file_get_contents($sFile), 1);
         if (!is_array($aMsg) || !array_key_exists('action', $aMsg)) {
             $this->_delete();
             return false;
         }
-        $this->_aMsg=$aMsg;
+        $this->_aMsg = $aMsg;
         return $aMsg['action'];
     }
 
     /**
-     * generate a touchfile
+     * Generate a touchfile (lock file)
      * @return string
      */
-    private function _getTouchfile(){
-        return dirname(__DIR__).'/data/ahcrawler_.lock';
+    private function _getTouchfile(): string
+    {
+        return dirname(__DIR__) . '/data/ahcrawler_.lock';
+        /*
         return is_writable(sys_get_temp_dir())
-            ? sys_get_temp_dir().'/ahcrawler_'.md5(__DIR__).'.lock'
-            : dirname(__DIR__).'/data/ahcrawler_.lock'
+            ? sys_get_temp_dir() . '/ahcrawler_' . md5(__DIR__) . '.lock'
+            : dirname(__DIR__) . '/data/ahcrawler_.lock'
         ;
+        */
     }
 
     /**
-     * delete touchfile
-     * @return boolean
+     * Delete touchfile
+     * @return boolean success
      */
-    private function _delete(){
+    private function _delete(): bool
+    {
         /*
         echo "STATUS: stopping " 
         . (array_key_exists('action', $this->_aMsg) ? $this->_aMsg['action'] : '')
@@ -96,19 +113,20 @@ class status {
          */
         return unlink($this->_getTouchfile());
     }
-    
+
     /**
-     * create or update touchfile
-     * @return boolean
+     * Create or update touchfile
+     * @return boolean|integer success
      */
-    private function _save(){
+    private function _save(): bool|int
+    {
         // echo "STATUS: running " . $this->_aMsg['action']."\n";
-        $sFile=$this->_getTouchfile();
-        
+        $sFile = $this->_getTouchfile();
+
         // $this->_aMsg['last']=microtime(1);
-        $this->_aMsg['last']=date("U");
+        $this->_aMsg['last'] = date("U");
         // file_put_contents does not update mtime ... so I delete it first
-        if (file_exists($sFile)){
+        if (file_exists($sFile)) {
             unlink($sFile);
         }
         return file_put_contents($sFile, json_encode($this->_aMsg));
@@ -118,77 +136,94 @@ class status {
     // ----------------------------------------------------------------------
 
     /**
-     * start an action and save current action as running process
+     * Start an action and save current action as running process
+     * 
      * @param string $sMsgId  name of the process
      * @return boolean
      */
-    public function startAction($sMsgId){
-        $aCurrent=$this->_getAction();
-        if($aCurrent){
+    public function startAction(string $sMsgId): bool
+    {
+        $aCurrent = $this->_getAction();
+        if ($aCurrent) {
             return false;
         }
-        $this->_aMsg=array(
-            'action'=>$sMsgId,
-            'start'=>date("U"),
-            'last'=>date("U"),
-        );
-        $this->_save();
-        return true;
-    }
-    
-    /**
-     * update .. set info that a process is still running
-     * @param string $sMsgId  name of the process
-     * @return boolean
-     */
-    public function updateAction($sMsgId, $sMessage){
-        $aCurrent=$this->_getAction();
-        if($aCurrent !== $sMsgId){
-            return false;
-        }
-        $this->_aMsg['lastmessage']=$sMessage;
+        $this->_aMsg = [
+            'action' => $sMsgId,
+            'start' => date("U"),
+            'last' => date("U"),
+        ];
         $this->_save();
         return true;
     }
 
     /**
-     * done ... finish the process
+     * Update .. set info that a process is still running
+     * @param string $sMsgId   name of the process
+     * @param string $sMessage message text
+     * @return boolean
+     */
+    public function updateAction(string $sMsgId, string $sMessage): bool
+    {
+        $aCurrent = $this->_getAction();
+        if ($aCurrent !== $sMsgId) {
+            return false;
+        }
+        $this->_aMsg['lastmessage'] = $sMessage;
+        $this->_save();
+        return true;
+    }
+
+    /**
+     * Done ... finish the process and delete lock file
      * @param string $sMsgId  name of the process
      * @return boolean
      */
-    public function finishAction($sMsgId){
-        $aCurrent=$this->_getAction();
-        if($aCurrent !== $sMsgId){
+    public function finishAction($sMsgId): bool
+    {
+        $aCurrent = $this->_getAction();
+        if ($aCurrent !== $sMsgId) {
             return false;
         }
         return $this->_delete();
     }
 
     /**
-     * get status about a (maybe) currently running action
-     * @return array
+     * Get status about a (maybe) currently running action
+     * It returns false if it is not locked.
+     * 
+     * @return bool|array
      */
-    public function getStatus(){
-        $aCurrent=$this->_getAction();
-        if(!$aCurrent){
+    public function getStatus(): bool|array
+    {
+        $aCurrent = $this->_getAction();
+        if (!$aCurrent) {
             return false;
         }
         return $this->_aMsg;
     }
 
-    public function showStatus(){
-        $aCurrent=$this->_getAction();
-        if(!$aCurrent){
+    /**
+     * Show locking status with 
+     * - current action
+     * - last response
+     * - running time
+     * 
+     * @return bool
+     */
+    public function showStatus(): bool
+    {
+        $aCurrent = $this->_getAction();
+        if (!$aCurrent) {
             echo "STATUS: no running action\n";
             return false;
         }
         echo "STATUS: running action: "
-        . "{$this->_aMsg['action']} \n"
-        . "- started       ".date("Y-d-m H:i:s", $this->_aMsg['start']). " \n"
-        . "- last response ".date("Y-d-m H:i:s", $this->_aMsg['last']). " "
-        . "(". (date("U") - $this->_aMsg['last']). " sec ago ... ignoring it after ".$this->_iTimeout." sec) \n"
-        . "- running for   ".($this->_aMsg['last']-$this->_aMsg['start'])." sec\n"
-        . "\n";
+            . "{$this->_aMsg['action']} \n"
+            . "- started       " . date("Y-d-m H:i:s", $this->_aMsg['start']) . " \n"
+            . "- last response " . date("Y-d-m H:i:s", $this->_aMsg['last']) . " "
+            . "(" . (date("U") - $this->_aMsg['last']) . " sec ago ... ignoring it after " . $this->_iTimeout . " sec) \n"
+            . "- running for   " . ($this->_aMsg['last'] - $this->_aMsg['start']) . " sec\n"
+            . "\n";
         return true;
     }
 }
