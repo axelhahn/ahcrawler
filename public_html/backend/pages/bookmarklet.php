21d915087164875ab84293239735543c6fabb5d3
Axel Hahn <ax2002@gmx.net>
2025-03-16 23:38:57 +0100

    beautify bookmarklets page

--- public_html/backend/pages/bookmarklet.php
+++ public_html/backend/pages/bookmarklet.php
@@ -5,6 +5,7 @@
 $sReturn = '';
 
 $oRenderer=new ressourcesrenderer();
+/*
 $sReturn.=''
         .'<h3>'.$this->lB('bookmarklet.details.head').'</h3>'
             . $oRenderer->renderBookmarklet('details')
@@ -19,5 +20,29 @@ $sReturn.=''
             .'<br><br>'
             .$this->lB('bookmarklet.sslcheck.posthint2')
         ;
+*/
+$sReturn.='<br><br><br><div class="pure-g">
+    <div class="pure-u-1-5 w30">
+        <h3>'.$this->lB('bookmarklet.details.head').'</h3>'
+            . $oRenderer->renderBookmarklet('details').'
+    </div>
+    <div class="">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div>
+
+    <div class="pure-u-1-5 w30">
+        <h3>'.$this->lB('bookmarklet.httpheaderchecks.head').'</h3>'
+                .$oRenderer->renderBookmarklet('httpheaderchecks')
+                .'<br><br>'
+                .$this->lB('bookmarklet.httpheaderchecks.posthint2').'
+    </div>
+    <div class="">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div>
+
+    <div class="pure-u-1-5 w30">
+        <h3>'.$this->lB('bookmarklet.sslcheck.head').'</h3>'
+            .$oRenderer->renderBookmarklet('sslcheck')
+            .'<br><br>'
+            .$this->lB('bookmarklet.sslcheck.posthint2').'
+    </div>
+
+</div>';
 
 return $sReturn;
