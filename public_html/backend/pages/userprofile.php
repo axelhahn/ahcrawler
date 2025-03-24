c06e51c400085c68767a7c459506dbf2d71400bf
Axel Hahn <ax2002@gmx.net>
2025-03-25 00:47:03 +0100

    update user profile page

--- public_html/backend/pages/userprofile.php
+++ public_html/backend/pages/userprofile.php
@@ -5,32 +5,32 @@
 
 $sReturn = '';
 
-$sGlobalTable='';
-$sGroupTable='';
+$sGlobalTable = '';
+$sGroupTable = '';
 
 
 
 
-$sGroups='';
-foreach($this->acl->getGroups() as $sGroup){
-    $sGroups.=$this->_getIcon('usergroup') . $sGroup .'<br>';
+$sGroups = '';
+foreach ($this->acl->getGroups() as $sGroup) {
+    $sGroups .= $this->_getIcon('usergroup') . $sGroup . '<br>';
 }
 
-$sGroups=$sGroups ? "<h4>".$this->lB("userprofile.groups")."</h4>$sGroups" : '';
+$sGroups = $sGroups ? "<h4>" . $this->lB("userprofile.groups") . "</h4>$sGroups" : '';
 // --- global permissions
 
-$aGlobalPerms=$this->acl->getMyGlobalPermissions();
+$aGlobalPerms = $this->acl->getMyGlobalPermissions();
 // print_r($aGlobalPerms);
 
-if(count($aGlobalPerms)){
-    $sGlobal='';
-    foreach($aGlobalPerms as $sPerm=>$bActive){
-        $sGlobal.=$bActive
-            ? (($sGlobal?', ':"" ) . "<button class=\"pure-button button-success\">X</button> $sPerm")
+if (count($aGlobalPerms)) {
+    $sGlobal = '';
+    foreach ($aGlobalPerms as $sPerm => $bActive) {
+        $sGlobal .= $bActive
+            ? (($sGlobal ? ', ' : "") . "<button class=\"pure-button button-success\">X</button> $sPerm")
             : '-'
-            ;
+        ;
     }
-    $sGlobal="<h4>".$this->lB("userprofile.globalperms")."</h4>
+    $sGlobal = "<h4>" . $this->lB("userprofile.globalperms") . "</h4>
         You have the following global permissions on each project:<br>
         <br>
         $sGlobal
@@ -40,64 +40,71 @@ if(count($aGlobalPerms)){
 
 // --- app specific permissions
 
-$aPerms=["admin","manager","viewer"];
-$sTHead='<tr><th>Name</th>';
-foreach($aPerms as $sPerm){
-    $sTHead.='<th>'.$sPerm.'</th>';
+$aPerms = ["admin", "manager", "viewer"];
+$sTHead = '<tr><th>Name</th>';
+foreach ($aPerms as $sPerm) {
+    $sTHead .= '<th>' . $sPerm . '</th>';
 }
-$sTHead.='</tr>';
+$sTHead .= '</tr>';
 
-foreach($this->_getProfiles() as $iGroupId => $sGroupName){
-    if($this->acl->is($iGroupId)){
-        $sGroupTable.='<tr><td>'.$this->_getIcon('project') . $sGroupName.'</td>';
-        foreach($aPerms as $sPerm){
-            $sShownPermission=$this->acl->is("{$iGroupId}_{$sPerm}")
+foreach ($this->_getProfiles() as $iGroupId => $sGroupName) {
+    if ($this->acl->is($iGroupId)) {
+        $sGroupTable .= '<tr><td>' . $this->_getIcon('project') . $sGroupName . '</td>';
+        foreach ($aPerms as $sPerm) {
+            $sShownPermission = $this->acl->is("{$iGroupId}_{$sPerm}")
                 ? "<button class=\"pure-button button-success\">X</button>"
                 : "<button class=\"pure-button button-error\">-</button>"
-                ;
-            $sGroupTable.='<td>'.$sShownPermission.'</td>';
+            ;
+            $sGroupTable .= '<td>' . $sShownPermission . '</td>';
         }
-        $sGroupTable.='</tr>';
+        $sGroupTable .= '</tr>';
     }
 }
-$sGroupTable=$sGroupTable 
-    ? '<h4>'.$this->lB("userprofile.webperms").'</h4>
+$sGroupTable = $sGroupTable
+    ? '<h4>' . $this->lB("userprofile.webperms") . '</h4>
         <table class="pure-table pure-table-horizontal datatable dataTable no-footer">
-        <thead>'.$sTHead.'</thead>
-        <tbody>'.$sGroupTable.'
+        <thead>' . $sTHead . '</thead>
+        <tbody>' . $sGroupTable . '
         </tbody>
-        </table><br><br>' 
+        </table><br><br>'
     : ''
-    ;
+;
 
 // --- output
 
-$sReturn.=$this->_getButton([
+$sReturn .= $this->_getButton([
     'href' => 'javascript:history.back();',
     // 'class' => 'button-secondary',
     'popup' => false,
     'label' => 'button.back'
-  ])  
-  .'<h3>'.$this->_getIcon('userprofile') . $this->_getUser().'</h3>'
-  .$sGroups
-  .$sGlobal
-  .$sGroupTable
-  /*
-  .'<pre>Groups:<br>'
-  .print_r($this->acl->getGroups(), 1)
-  .'<hr>'
-  .print_r($this->_getProfiles(), 1)
-  .'</pre>'
-  */
-  ;
+])
+    . ' '
+    . $this->_getButton([
+        'href' => './?page=logoff',
+        'class' => 'button-secondary',
+        'label' => 'button.logoff',
+        'popup' => false
+    ])
+    . '<h3>' . $this->_getIcon('userprofile') . $this->_getUser() . '</h3>'
+    . $sGroups
+    . $sGlobal
+    . $sGroupTable
+    /*
+    .'<pre>Groups:<br>'
+    .print_r($this->acl->getGroups(), 1)
+    .'<hr>'
+    .print_r($this->_getProfiles(), 1)
+    .'</pre>'
+    */
+;
 
 $sReturn .= '<hr>'
-. $this->_getButton([
-  'href' => './?page=logoff',
-  'class' => 'button-secondary',
-  'label' => 'button.logoff',
-  'popup' => false
-]);
+    . $this->_getButton([
+        'href' => './?page=logoff',
+        'class' => 'button-secondary',
+        'label' => 'button.logoff',
+        'popup' => false
+    ]);
 
 return $sReturn;
 
